8b661a547eb8f9fc6011af95f2c79a3baa83622b
shupil <clanguagemaker@gmail.com>
2025-08-27 15:17:15 +0900

    2025.8.27

---
+++ chap02/src/sec04/exam03/ContinueKeyCodeReadExample.java
@@ -0,0 +1,16 @@
+package sec04.exam03;
+
+import java.io.IOException;
+
+public class ContinueKeyCodeReadExample {
+
+	public static void main(String[] args) throws IOException {
+		int keyCode;
+		while(true) {
+			keyCode = System.in.read();
+			System.out.println("keyCode: " + keyCode);
+		}
+
+	}
+
+}
