22e3f6edf9e3af0811393d63af0b54f1403f8785
shupil <clanguagemaker@gmail.com>
2025-08-27 15:43:32 +0900

    2025.8.27

---
+++ chap02/src/sec01/exam01/VariableInitializationExample.java
@@ -0,0 +1,11 @@
+package sec01.exam01;
+
+public class VariableInitializationExample {
+
+	public static void main(String[] args) {
+		int value = 10; // 정의 필요
+		int result = value + 10;
+		System.out.println(result);
+	}
+
+}
