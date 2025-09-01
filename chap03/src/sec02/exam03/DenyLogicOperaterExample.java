a79c80413f12f64b1a33c4394978fbdad53b2d52
shupil <clanguagemaker@gmail.com>
2025-09-01 13:42:55 +0900

    .

---
+++ chap03/src/sec02/exam03/DenyLogicOperaterExample.java
@@ -0,0 +1,16 @@
+package sec02.exam03;
+
+public class DenyLogicOperaterExample {
+
+	public static void main(String[] args) {
+		boolean play = true;
+		System.out.println(play);
+		
+		play = !play;
+		System.out.println(play);
+		
+		play = !play;
+		System.out.println(play);
+	}
+
+}
