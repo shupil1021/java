7e2f2f5f7e4ddc05c5e94cb4dcd37bde824b42bf
shupil <clanguagemaker@gmail.com>
2025-09-03 15:28:26 +0900

    .

--- chap04/src/sec02/exam10/ContinueExample.java
+++ chap04/src/sec02/exam10/ContinueExample.java
@@ -3,8 +3,12 @@ package sec02.exam10;
 public class ContinueExample {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				continue;
+			}
+			System.out.println(i);
+		}
 	}
 
 }
